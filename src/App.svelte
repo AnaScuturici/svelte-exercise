4d1950c31003f016787396a2a63edfe2c777427b
ana scuturici <anascuturici@gmail.com>
2022-03-24 21:57:31 +0900

    add: conditionals

--- src/App.svelte
+++ src/App.svelte
@@ -9,12 +9,25 @@
 		people = people.filter(person => person.id !== id);
 	};
 
+	let num = 5;
+
 </script>
+
+{#if num > 20}
+	<p>Greater than 20</p>
+{:else if num > 5}
+	<p>Greater than 5</p>
+{:else}
+	<p>Not greater than 5</p>
+{/if}
 
 <main>
 	{#each people as person (person.id)}
 	<div>
 		<h4>{person.name}</h4>
+		{#if person.color === "black"}
+		<p><strong>master ninja</strong></p>
+		 {/if}
 		<p>{person.age} years old, {person.color} belt</p>
 		<button on:click={() => handleClick(person.id)}>Delete</button>
 	</div>
