6f2bff2090055acbdfdef2aee09df1252235e8aa
ana scuturici <anascuturici@gmail.com>
2022-03-24 23:30:29 +0900

    add: checkboxes to form

--- src/AddPersForm.svelte
+++ src/AddPersForm.svelte
@@ -2,13 +2,26 @@
     let name;
     let color;
     let age;
+    let fighting = false;
+    let running = false;
+    let sneaking = false;
+
+    const handleSubmit = () => {
+        console.log(name, color, age, fighting, running, sneaking);
+    }
+
 </script>
 
 <h3>Add new person</h3>
-<form>
+<!--preventDefault event modifier-->
+<form on:submit|preventDefault={handleSubmit}>
     <input type="text" placeholder="name" bind:value={name}>
     <input type="text" placeholder="color" bind:value={color}>
     <input type="number" placeholder="age" bind:value={age}>
+    <label>Skills:</label>
+    <input type="checkbox" bind:checked={fighting}>fighting<br>
+    <input type="checkbox" bind:checked={running}>running<br>
+    <input type="checkbox" bind:checked={sneaking}>sneaking<br>
     <button>Add person</button>
 </form>
 
