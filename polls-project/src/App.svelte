53b54eb9364dccc964a7f54de578233b5e318750
ana scuturici <anascuturici@gmail.com>
2022-03-26 22:26:56 +0900

    add: tabs component

--- polls-project/src/App.svelte
+++ polls-project/src/App.svelte
@@ -1,11 +1,17 @@
 <script>
 	import Header from "./components/Header.svelte";
 	import Footer from "./components/Footer.svelte";
+	import Tabs from "./shared/Tabs.svelte";
+
+	// tabs
+	let items = ["Current polls", "Add new poll"];
+	let activeItem = "Current polls";
+
 </script>
 
 <Header />
 <main>
-	
+	<Tabs items={items} activeItem={activeItem}/>
 </main>
 <Footer />
 
