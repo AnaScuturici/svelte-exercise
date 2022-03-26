181cff5e611b1597f777251c1293ab7f47303223
ana scuturici <anascuturici@gmail.com>
2022-03-26 22:04:07 +0900

    add: header component

--- polls-project/src/App.svelte
+++ polls-project/src/App.svelte
@@ -1,11 +1,15 @@
 <script>
-	
+	import Header from "./components/Header.svelte";
 </script>
 
+<Header />
 <main>
-	<h1>Hello!</h1>
+	<p>Test test test</p>
 </main>
 
 <style>
-
+	main {
+		max-width: 960px;
+		margin: 40px auto;
+	}
 </style>
---
+++ polls-project/src/components/Footer.svelte
@@ -0,0 +1,7 @@
+<footer>
+    <div class="copyright"> Copyright 2020 Poll Ninja</div>
+</footer>
+
+<style>
+    
+</style>
