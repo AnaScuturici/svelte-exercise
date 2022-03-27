eeacd3d1b41dfcd335254c20496f1d057c37cb62
ana scuturici <anascuturici@gmail.com>
2022-03-28 00:12:52 +0900

    add: transitions and animations to poll cards

--- polls-project/src/components/PollList.svelte
+++ polls-project/src/components/PollList.svelte
@@ -1,12 +1,15 @@
 <script>
+    import { fade, slide, scale } from "svelte/transition";
+    import { flip } from "svelte/animate";
     import PollStore from "../stores/pollStore.js";
     import PollDetail from "./PollDetail.svelte";
 
 </script>
 
 <div class="poll-list">
+    <!--local: only an individual item is scaled out-->
     {#each $PollStore as poll(poll.id)} 
-    <div>
+    <div in:fade out:scale|local animate:flip={{duration: 500}}>
         <PollDetail poll={poll}/>
     </div>
     {/each}
