559e3906e90408437a9b503ff322bdc416537555
ana scuturici <anascuturici@gmail.com>
2022-03-27 15:57:43 +0900

    add: style for poll details

--- polls-project/src/components/PollDetail.svelte
+++ polls-project/src/components/PollDetail.svelte
@@ -20,6 +20,28 @@ $: totalVotes = poll.votesA + poll.votesB;
 </div>
 
 <style>
-
+    h3 {
+        margin: 0 auto;
+        color: #555;
+    }
+    p {
+        margin-top: 6px;
+        font-size: 14px;
+        color: #aaa;
+        margin-bottom: 30px;
+    }
+    .answer {
+        background: #fafafa;
+        cursor: pointer;
+        margin: 10px auto;
+        position: relative;
+    }
+    .answer:hover {
+        opacity: 0.6;
+    }
+    span {
+        display: inline-block;
+        padding: 10px 20px;
+    }
 </style>
 
