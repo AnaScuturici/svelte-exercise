41330fa0b7ad6b1e88a821a20b8ca38c86096c4d
ana scuturici <anascuturici@gmail.com>
2022-03-27 15:51:41 +0900

    add: total votes in poll detail

--- polls-project/src/components/PollDetail.svelte
+++ polls-project/src/components/PollDetail.svelte
@@ -1,9 +1,14 @@
 <script>
     export let poll;
+
+    //reactive values
+$: totalVotes = poll.votesA + poll.votesB;
+
 </script>
 
 <div class="poll">
     <h3>{ poll.question }</h3>
+    <p>Total votes: {totalVotes}</p>
     <div class="answer">
         <div class="percent percent-a"></div>
         <span>{ poll.answerA }({ poll.votesA })</span>
